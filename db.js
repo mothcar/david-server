ce98ee5988d6aa9c781f697bcd314cdbbda5cda1
Paul Hwang <mothcar@gmail.com>
2024-06-13 09:12:50 +0900

    Remove useNewUrlParser

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 // const MONGO_URI = 'mongodb://localhost/your-database-name';
 const MONGO_URI = 'mongodb+srv://rojinmothcar:1748rojin@cluster0.3byf2fd.mongodb.net/monkey?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
